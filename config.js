009d14f090a26494875dd9f5d83ef791b84788cc
Killsecurly-foundation <ksf.paulf@outlook.com>
2024-12-04 14:43:27 -0800

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,19 @@ const config = {
   users: {
     // You can add multiple users by doing username: 'password'.
     interstellar: "38944807!!",
+    MAHIDHARS : "371505",
+    NOAHL : "371979",
+    MINHM : "393539",
+    YUGK : "393331",
+    JASONK : "393728",
+    RYANK : "378527",
+    YAIRS : "377598",
+    YEHONATHANT : "386396",
+    YICONGW : "390287",
+    SHREYANS : "372069",
+    YONATHANG : "368713",
+    SWAYAMS : "368894",
+    SNEHIND : "376250",
   },
 };
 
